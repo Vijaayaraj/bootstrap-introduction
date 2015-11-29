8c3d0aa3f6660d4d45c8de9a8da53907a7e2ee1b
Vijayaraj <mvijayaraj4uvijay@gmail.com>
2015-11-29 10:57:39 +0530

    first commit

---
+++ BootstrapIntroduction/DAL/BookInitializer.cs
@@ -0,0 +1,66 @@
+﻿using BootstrapIntroduction.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BootstrapIntroduction.DAL
+{
+    public class BookInitializer : DropCreateDatabaseIfModelChanges<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            var author = new Author
+            {
+                Biography = "...",
+                FirstName = "Jamie",
+                LastName = "Munro"
+            };
+
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    Author = author,
+                    Description = "...",
+                    ImageUrl= "http://ecx.images-amazon.com/images/I/512IS3Q3ywL._SX403_BO1,204,203,200_.jpg",
+                    Isbn = "233452345435",
+                    Synopsis = "...",
+                    Title = "Getting Started with Knockout.js for .NET Developers"
+                },
+                new Book
+                {
+                    Author = author,
+                    Description = "...",
+                    ImageUrl= "http://ecx.images-amazon.com/images/I/512IS3Q3ywL._SX403_BO1,204,203,200_.jpg",
+                    Isbn = "233452345435",
+                    Synopsis = "...",
+                    Title = "Getting Started with Knockout.js for .NET Developers"
+                },
+                new Book
+                {
+                    Author = author,
+                    Description = "...",
+                    ImageUrl= "http://ecx.images-amazon.com/images/I/512IS3Q3ywL._SX403_BO1,204,203,200_.jpg",
+                    Isbn = "233452345435",
+                    Synopsis = "...",
+                    Title = "Getting Started with Knockout.js for .NET Developers"
+                },
+                new Book
+                {
+                    Author = author,
+                    Description = "...",
+                    ImageUrl= "http://ecx.images-amazon.com/images/I/512IS3Q3ywL._SX403_BO1,204,203,200_.jpg",
+                    Isbn = "233452345435",
+                    Synopsis = "...",
+                    Title = "Getting Started with Knockout.js for .NET Developers"
+                }
+            };
+
+            books.ForEach(b => context.Books.Add(b));
+
+            context.SaveChanges();            
+        }
+    }
+}
---
+++ BootstrapIntroduction/Models/QueryOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootstrapIntroduction.Models
+{
+    public class QueryOptions
+    {
+        public QueryOptions()
+        {
+           
+        }
+
+        public string SortField { get; set; }
+        public SortOrder SortOrder { get; set; }
+
+        public string Sort
+        {
+            get
+            {
+                return string.Format("{0} {1}", SortField, SortOrder.ToString());
+            }
+        }
+
+    }
+}
